0389457c164a83adb6ad72382699e4ac6020a6d0
Yashsadarangani <yash.sadarangani2021@vitbhopal.ac.in>
2024-12-21 21:29:50 +0530

    FB series in iteration

--- fibb.py
+++ fibb.py
@@ -1,12 +1,17 @@
-def fibonacci(n):
+def fibonacci_series(n):
     if n <= 0:
-        return 0
+        return []
     elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_series = [0, 1]
+    for i in range(2, n):
+        next_term = fib_series[-1] + fib_series[-2]
+        fib_series.append(next_term)
+    return fib_series
 num_terms = int(input("Enter the number of terms: "))
 
 print("Fibonacci Series:")
-for i in range(num_terms):
-    print(fibonacci(i), end=" ")
+print(" ".join(map(str, fibonacci_series(num_terms))))
